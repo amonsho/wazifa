63e1b35b76a2cc16b50564fdf498227399ad6610
amonsho <amonsho004@gmail.com>
2025-06-12 18:52:49 +0500

    add1

--- zadachahoiFor/tasks1.py
+++ zadachahoiFor/tasks1.py
@@ -1,6 +1,14 @@
-n = int(input())
+# n = int(input())
 
-for i in range(1, n + 1):
-    a = ' ' * (n - i)        
-    z= '*' * (2 * i - 1) 
-    print(a + z)
+# for i in range(1, n + 1):
+#     a = ' ' * (n - i)        
+#     z= '*' * (2 * i - 1) 
+#     print(a + z)
+
+n=int(input())
+k=n-1
+l=1
+for i in range(1, n+1):
+    print(' '*k,l*'*')
+    k-=1
+    l+=2
